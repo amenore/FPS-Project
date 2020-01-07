9e634322750e389b177120bfa41e47a13b11749a
amenore <c.shafer2@students.clark.edu>
2020-01-06 16:42:39 -0800

    initialized repository

---
+++ Assets/scripts/BulletScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    bool hasImpacted = false;
+    public LayerMask layerMask = 1;
+    float timeSinceImpact;
+
+    public ParticleSystem impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.impactEffect.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (hasImpacted)
+        {
+            timeSinceImpact += Time.deltaTime;
+        }
+        if (timeSinceImpact >= .1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {   
+        hasImpacted = true;
+        print(hasImpacted);
+        this.impactEffect.gameObject.SetActive(true);
+        this.GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+    }
+
+    
+}
---
+++ Assets/scripts/GunController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public Transform gunTransform;
+
+    public bool isAutomatic;
+
+    public GameObject projectile;
+
+    public AudioSource gunRacking;
+    public AudioSource gunfire;
+    public AudioSource gunfireTail;
+    public AudioSource casingFalling;
+
+    public ParticleSystem muzzleFlash;
+
+    public int fireRate;
+    float shotsPerSecond;
+    public float bulletVelocity;
+
+    float timeSinceFire;
+
+    bool isADS = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotsPerSecond = fireRate / 60;
+        gunRacking.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        muzzleFlash.Stop();
+        if (isAutomatic) {
+            if (Input.GetMouseButton(0) && timeSinceFire >= 1 / shotsPerSecond)
+            {
+                projectileFire();
+                gunfire.Play();
+                gunfireTail.Play();
+
+                muzzleFlash.Play();
+                timeSinceFire = 0;
+            }
+            timeSinceFire += Time.deltaTime;
+        }
+
+        else if (!isAutomatic)
+        {
+            if (Input.GetMouseButtonDown(0) && timeSinceFire >= 1 / shotsPerSecond)
+            {
+                projectileFire();
+                gunfire.Play();
+                gunfireTail.Play();
+
+                muzzleFlash.Play();
+                timeSinceFire = 0;
+            }
+            timeSinceFire += Time.deltaTime;
+        } 
+    }
+
+    void rayCastFire()
+    {
+
+    }
+    void projectileFire()
+    {
+        GameObject bullet = Instantiate(projectile, new Vector3(transform.position.x, transform.position.y, transform.position.z), gunTransform.rotation) as GameObject;
+        bullet.SetActive(true);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletVelocity);
+    }
+    
+}
